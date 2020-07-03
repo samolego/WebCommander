0ff836a5d0039db4aa96de8f34f9a993a049074c
pg008 <67679803+pg008@users.noreply.github.com>
2020-07-03 17:17:42 +0200

    Update webcommander.js

--- webcommander.js
+++ webcommander.js
@@ -116,7 +116,7 @@ class WebCommander {
 			this.consoleHiddenInput = document.createElement("INPUT");
 			this.consoleHiddenInput.style.position = "relative";
 			this.consoleHiddenInput.style.top = "-50px";
-			this.consoleHiddenInput.style.opacity = 0;
+			this.consoleHiddenInput.style.opacity = 1;
 		}
 		
 		// Focus the input field on click
